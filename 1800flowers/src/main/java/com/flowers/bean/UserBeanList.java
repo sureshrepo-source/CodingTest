a3c7f5c301faeb5d05f2ce8ea13f3f13a85697c1
Suresh Karnati <sureshkarnati24@gmail.com>
2019-08-05 16:00:00 +0530

    Initial commit coding test

---
+++ 1800flowers/src/main/java/com/flowers/bean/UserBeanList.java
@@ -0,0 +1,22 @@
+package com.flowers.bean;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Suresh Karnati
+ * UserBeanList.java
+ * v1.0
+ *
+ */
+public class UserBeanList {
+	private List<User> list;
+
+	public List<User> getList() {
+		return list;
+	}
+
+	public void setList(List<User> list) {
+		this.list = list;
+	}
+}
---
+++ 1800flowers/src/main/java/com/flowers/bean/UsersResponse.java
@@ -0,0 +1,15 @@
+package com.flowers.bean;
+
+public class UsersResponse {
+	private int totalUsers;
+
+	public int getTotalUsers() {
+		return totalUsers;
+	}
+
+	public void setTotalUsers(int totalUsers) {
+		this.totalUsers = totalUsers;
+	}
+	
+
+}
